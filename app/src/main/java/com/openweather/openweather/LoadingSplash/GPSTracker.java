00185f70d5df3939f3d643a5254e1dc0cd07e731
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2017-02-26 16:36:40 +0800

    GPS經緯度，並偵測是否開啟GPS以及Android6.0權限提示 Log出國家、縣市、區、里、地址

---
+++ app/src/main/java/com/openweather/openweather/LoadingSplash/GPSTracker.java
@@ -0,0 +1,170 @@
+package com.openweather.openweather.LoadingSplash;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+import static android.os.Build.VERSION_CODES.M;
+
+/**
+ * Created by andy6804tw on 2017/2/26.
+ */
+
+public class GPSTracker extends Service implements LocationListener {
+    private final Context context;
+    boolean isGPSEnable=false;
+    boolean isNetworkEnabled=false;
+    boolean canGetLocation=false;
+    Location location;
+    double latitude;
+    double longtitude;
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES=10;
+    private static final long MIN_TIME_BW_UPDATES=1000*60*1;
+    protected LocationManager locationManager;
+    public GPSTracker(Context context){
+        this.context=context;
+        getLocation();
+    }
+    public Location getLocation(){
+        try{
+            locationManager=(LocationManager)context.getSystemService(LOCATION_SERVICE);
+            isGPSEnable=locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            isNetworkEnabled=locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            if(!isGPSEnable&&isNetworkEnabled){
+                Log.e("Data55","2");
+            }
+            else{
+                    this.canGetLocation=true;
+
+                    if(isNetworkEnabled){
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,MIN_TIME_BW_UPDATES,MIN_DISTANCE_CHANGE_FOR_UPDATES,this);
+                }
+                if(locationManager!=null){
+                    location=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    if(location!=null){
+                        latitude=location.getLatitude();
+                        longtitude=location.getLongitude();
+                    }
+                }
+                if(isGPSEnable){
+                    if(location==null){
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,MIN_TIME_BW_UPDATES,MIN_DISTANCE_CHANGE_FOR_UPDATES,this);
+                        if(locationManager!=null){
+                            location=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if(location!=null){
+                                latitude=location.getLatitude();
+                                longtitude=location.getLongitude();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return location;
+    }
+
+    private void requestPermissions(String[] strings, int requestCode) {
+        switch (requestCode) {
+            case 10:
+                // 如果裝置版本是6.0（包含）以上
+                if (Build.VERSION.SDK_INT >= M) {
+                    // 取得授權狀態，參數是請求授權的名稱
+                    checkSelfPermission(
+                            Manifest.permission.ACCESS_FINE_LOCATION);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void stopUsingGPS(){
+
+        if(locationManager!=null){
+            locationManager.removeUpdates(GPSTracker.this);
+        }
+    }
+
+    public  double getLatitude(){
+        if(location!=null){
+            latitude=location.getLatitude();
+        }
+        return  latitude;
+    }
+
+    public double getLongtitude(){
+        if(location!=null){
+            longtitude=location.getLongitude();
+        }
+        return  longtitude;
+    }
+
+    public boolean isCanGetLocation(){
+        return this.canGetLocation;
+    }
+
+    public void showSettingsAlert(){
+        AlertDialog.Builder alertDialog=new AlertDialog.Builder(context);
+        alertDialog.setTitle("Gps is settings");
+        alertDialog.setMessage("Gps is not enabled.");
+        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent=new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                context.startActivity(intent);
+            }
+        });
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        alertDialog.show();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
+
+
--- app/src/main/java/com/openweather/openweather/LoadingSplash/SplashActivity.java
+++ app/src/main/java/com/openweather/openweather/LoadingSplash/SplashActivity.java
@@ -1,6 +1,15 @@
 package com.openweather.openweather.LoadingSplash;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.WindowManager;
@@ -19,7 +28,12 @@ import com.openweather.openweather.View.SunBabyLoadingView;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import static android.os.Build.VERSION_CODES.M;
+
 public class SplashActivity extends AppCompatActivity {
+
+    GPSTracker mGps;
+    int count=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,19 +42,62 @@ public class SplashActivity extends AppCompatActivity {
         ExitApplication.getInstance().addActivity(this);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+
+        GPSPremessionCheck();
+
+    }
+
+    private void GPSPremessionCheck() {
+        /**偵測權限**/
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //如果沒有授權使用定位就會跳出來這個
+            // TODO: Consider calling
+            Log.e("Data6", "進入!");
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            // 如果裝置版本是6.0（包含）以上
+            if (Build.VERSION.SDK_INT >= M) {
+                // 取得授權狀態，參數是請求授權的名稱
+                int hasPermission = checkSelfPermission(
+                        Manifest.permission.ACCESS_FINE_LOCATION);
+                // 如果未授權
+                if (hasPermission != PackageManager.PERMISSION_GRANTED) {
+                    // 請求授權
+                    //     第一個參數是請求授權的名稱
+                    //     第二個參數是請求代碼
+                    Log.e("Data3", "失敗!");
+                    requestPermissions(
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            10);
+                } else
+                    Log.e("Data4", "成功!");
+            }
+        }
+        /***/
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        init_GPS();
+        init_Weather();
+                onRestart();
+
+    }
+
+    private void init_Weather() {
         ///**撈取資料END**///
         ///**撈取天氣資料START**///
         // Instantiate the RequestQueue.
-        RequestQueue queue2 = Volley.newRequestQueue(this);
-        String url2 ="https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(SELECT%20woeid%20FROM%20geo.places%20WHERE%20text%3D\"(22.9097,120.275002)\")&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(SELECT%20woeid%20FROM%20geo.places%20WHERE%20text%3D\"(22.9097,120.275002)\")&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 
         // Request a string response from the provided URL.
-        StringRequest stringRequest2 = new StringRequest(Request.Method.GET, url2,
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
@@ -51,27 +108,27 @@ public class SplashActivity extends AppCompatActivity {
                             jsonObject = new JSONObject(response);
 
                             //風 wind
-                            String chill=jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("wind").getString("chill");
-                            String direction=jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("wind").getString("direction");
-                            String speed=jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("wind").getString("speed");
+                            String chill = jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("wind").getString("chill");
+                            String direction = jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("wind").getString("direction");
+                            String speed = jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("wind").getString("speed");
                             //大氣 Atmosphere
-                            String humidity=jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("atmosphere").getString("humidity");
-                            String pressure=jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("atmosphere").getString("pressure");
-                            String rising=jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("atmosphere").getString("rising");
-                            String visibility=jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("atmosphere").getString("visibility");
+                            String humidity = jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("atmosphere").getString("humidity");
+                            String pressure = jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("atmosphere").getString("pressure");
+                            String rising = jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("atmosphere").getString("rising");
+                            String visibility = jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("atmosphere").getString("visibility");
                             //天文 Astronomy
-                            String sunrise=jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("astronomy").getString("sunrise");
-                            String sunset=jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("astronomy").getString("sunset");
+                            String sunrise = jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("astronomy").getString("sunrise");
+                            String sunset = jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("astronomy").getString("sunset");
                             //狀態 Condition
-                            String date=jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("item").getJSONArray("forecast").getJSONObject(0).getString("date");
-                            String day=jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("item").getJSONArray("forecast").getJSONObject(0).getString("day");
-                            String high=jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("item").getJSONArray("forecast").getJSONObject(0).getString("high");
-                            String low=jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("item").getJSONArray("forecast").getJSONObject(0).getString("low");
-                            String code=jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("item").getJSONArray("forecast").getJSONObject(0).getString("code");
-                            Log.e("wind",chill+" | "+direction+" | "+speed);
-                            Log.e("Atmosphere",humidity+" | "+pressure+" | "+" | "+rising+" | "+visibility);
-                            Log.e("Astronomy",sunrise+" | "+sunset);
-                            Log.e("Condition",date+" | "+day+" | "+" | "+high+" | "+low);
+                            String date = jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("item").getJSONArray("forecast").getJSONObject(0).getString("date");
+                            String day = jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("item").getJSONArray("forecast").getJSONObject(0).getString("day");
+                            String high = jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("item").getJSONArray("forecast").getJSONObject(0).getString("high");
+                            String low = jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("item").getJSONArray("forecast").getJSONObject(0).getString("low");
+                            String code = jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("item").getJSONArray("forecast").getJSONObject(0).getString("code");
+                            Log.e("wind", chill + " | " + direction + " | " + speed);
+                            Log.e("Atmosphere", humidity + " | " + pressure + " | " + " | " + rising + " | " + visibility);
+                            Log.e("Astronomy", sunrise + " | " + sunset);
+                            Log.e("Condition", date + " | " + day + " | " + " | " + high + " | " + low);
 
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -80,13 +137,79 @@ public class SplashActivity extends AppCompatActivity {
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(SplashActivity.this,"無法連接網路!",Toast.LENGTH_SHORT).show();
-                SunBabyLoadingView.str="正載入歷史資料...";
+                Toast.makeText(SplashActivity.this, "無法連接網路!", Toast.LENGTH_SHORT).show();
+                SunBabyLoadingView.str = "正載入歷史資料...";
             }
         });
         // Add the request to the RequestQueue.
-        queue2.add(stringRequest2);
+        queue.add(stringRequest);
+    }
 
-        ///**撈取資料END**///
+    private void init_GPS() {
+        mGps = new GPSTracker(this);
+        if (mGps.canGetLocation && mGps.getLatitude() != (0.0) && mGps.getLongtitude() != (0.0)) {
+            final double latitude = mGps.getLatitude();
+            final double longtitude = mGps.getLongtitude();
+            Toast.makeText(getApplicationContext(), "Your Location is->\nLat: " + latitude + "\nLong: " + longtitude, Toast.LENGTH_LONG).show();
+            ///**撈取時間資料START**///
+            // Instantiate the RequestQueue.
+            RequestQueue queue = Volley.newRequestQueue(this);
+            String url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude + "," + longtitude + "&language=zh-TW&sensor=true&key=AIzaSyDHA4UDKuJ_hZafj8Xn6m3mMzOsQnbTZ_w&lafhdfhfdhfdhrh";
+
+            // Request a string response from the provided URL.
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            // Display the first 500 characters of the response string.
+                            //mTextView.setText("Response is: "+ response.substring(0,500));
+                            JSONObject jsonObject = null;
+                            try {
+                                jsonObject = new JSONObject(response);
+                                int count = jsonObject.getJSONArray("results").getJSONObject(0).getJSONArray("address_components").length();
+                                String str = jsonObject.getJSONArray("results").getJSONObject(0).getJSONArray("address_components").getJSONObject(count - 3).getString("short_name");
+                                String str2 = jsonObject.getJSONArray("results").getJSONObject(0).getJSONArray("address_components").getJSONObject(count - 4).getString("short_name");
+                                String str3 = jsonObject.getJSONArray("results").getJSONObject(0).getJSONArray("address_components").getJSONObject(count - 5).getString("short_name");
+                                String str4 = jsonObject.getJSONArray("results").getJSONObject(0).getJSONArray("address_components").getJSONObject(count - 2).getString("long_name");
+                                String str5 = jsonObject.getJSONArray("results").getJSONObject(0).getString("formatted_address");
+                                Log.e("Data", str);
+                                Log.e("Data2", str2);
+                                Log.e("Data3", str3);
+                                Log.e("Data4", str4);
+                                Log.e("Data5", str5);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                }
+            });
+            // Add the request to the RequestQueue.
+            queue.add(stringRequest);
+        }else{
+            final LocationManager locationManager=(LocationManager)getSystemService(LOCATION_SERVICE);
+            if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+                AlertDialog.Builder alertDialog=new AlertDialog.Builder(this);
+                alertDialog.setTitle("Gps is settings");
+                alertDialog.setMessage("Gps is not enabled.");
+                alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent=new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(intent);
+                    }
+                });
+                alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                alertDialog.setCancelable(false);
+                alertDialog.show();
+            }
+        }
     }
 }
